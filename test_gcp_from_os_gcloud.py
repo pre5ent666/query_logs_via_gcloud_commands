a476ea02354563e8ca4903f8ebc2fff7e157b696
pre5ent666 <75908533+pre5ent666@users.noreply.github.com>
2024-02-05 10:34:25 +0800

    optimized

--- test_gcp_from_os_gcloud.py
+++ test_gcp_from_os_gcloud.py
@@ -17,8 +17,10 @@ def query_gcp(device_id, event_type, limit=5):
     
 def query_gcp_ts(device_id, event_type, ts1, ts2, limit=5):
     query =f'jsonPayload.type="{event_type}" AND jsonPayload.device="{device_id}" AND timestamp>="{ts1}" AND timestamp<="{ts2}"'
+    cmd = ["gcloud", "logging", "read", query, "--limit", str(limit), "--format", "json"]
     try:
-        byteOutput = subprocess.check_output(["gcloud", "logging", "read", query, "--limit", str(limit), "--format", "json"], timeout=10)
+        byteOutput = subprocess.check_output(cmd, timeout=10)
+        print(f"Execute the following command\n{' '.join(cmd)}")
         rlts = json.loads(byteOutput.decode('UTF-8').rstrip())
         return rlts
     except subprocess.CalledProcessError as e:
@@ -28,21 +30,16 @@ def query_gcp_ts(device_id, event_type, ts1, ts2, limit=5):
 if __name__ == "__main__":
     x = 1800
     START_TIME = time.time_ns()
-    # gte = np.datetime_as_string(np.datetime64(int(START_TIME / 1e6), "ms"), timezone="UTC")
-    # print(gte)
     ts2 = np.datetime_as_string(np.datetime64(int(START_TIME), "ns"), timezone="UTC")
-    print(ts2)
+    # print(ts2)
     ts1 = np.datetime_as_string(np.datetime64(int(START_TIME - x*1e9), "ns"), timezone="UTC")
-    print(ts1)
-
+    # print(ts1)
 
     # time.sleep(500)
 
     device_id = "5efc568967354e8d"
     # event_type = "watchdog-report-summary"
     event_type = "vehicle-state"
-
-
     # j = query_gcp(device_id, event_type, 1)
     # print(j[0]['jsonPayload']['parameters']['imei'])
     # print(j[0]['jsonPayload']['parameters']['data']['current_vehicle_state'])
